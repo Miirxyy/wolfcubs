363776409b75ef4885f547b51445c667d910fa4a
Farisah <farisahthira@gmail.com>
2023-05-15 21:37:43 +0800

    'lobby'

---
+++ src/warewolfculb/Lobby.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package warewolfculb;
+
+import java.util.*;
+import java.util.concurrent.locks.*;
+
+/**
+ *
+ * @author Farisah
+ */
+public class Lobby{
+    private static int userIdStart = 0;
+    private static int roomCounter = 0;
+    private static int[] user;
+    private static final int MAX_PUBLIC_ROOMS = 10;
+    private static final int MAX_PRIVATE_ROOMS = 10;
+
+    private static final List<Room> publicRooms = new ArrayList<>();
+    private static final List<Room> privateRooms = new ArrayList<>();
+    private static final Lock publicRoomsLock = new ReentrantLock();
+    private static final Lock privateRoomsLock = new ReentrantLock();
+
+    //to generate user ID
+    public static int generateUserId() {
+        synchronized (Lobby.class) {
+            userIdStart = ++userIdStart;
+            return userIdStart;
+        }
+    }
+    
+    public static int getUserId(){
+        return userIdStart;
+    }
+    
+    //If user join public room
+    //
+    public static void joinPublicRoom(int userId) {
+        publicRoomsLock.lock();
+        try {
+            //find available room
+            for (Room room : publicRooms) {
+                if (!room.isFull()) {
+                    room.addUser(userId);
+                    return;
+                }
+            }
+            // If no available public room, create new one
+            if (publicRooms.size() < MAX_PUBLIC_ROOMS) {
+                Room room = new Room();
+                room.addUser(userId);
+                publicRooms.add(room);
+                new Thread(room).start(); // Start the room as a thread
+            } else {
+                // If all public rooms are full
+            }
+        } finally {
+            publicRoomsLock.unlock();
+        }
+    }
+    
+    //if user create private room
+    //user can join room code 2 even if there is no room 2 created before!!!!!!!!!!!!!!
+    //private room would be automatically destroyed if more than 5 min waiting time
+    public static String createPrivateRoom(int userId) {
+        privateRoomsLock.lock();
+        try {
+            if (privateRooms.size() >= MAX_PRIVATE_ROOMS) {
+                // TODO: Handle the case when all private rooms are full
+                return null;
+            }
+            Room room = new Room();
+            room.addUser(userId);
+            privateRooms.add(room);
+            new Thread(room).start();
+            return String.format("%04d", ++roomCounter);
+        } finally {
+            privateRoomsLock.unlock();
+        }
+    }
+    
+    //fullPrivateRoom return 1 if full, return 0 if still not full
+    public static int fullPrivateRoom(int roomcode){
+        //accessing the database, get total user in the room or maybe we can use map first
+        return(0);
+    }
+}
---
+++ src/warewolfculb/LobbyInterfaceController.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package warewolfculb;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Farisah
+ */
+public class LobbyInterfaceController {     // implements Initializable
+
+    private Lobby lobby; // Reference to the Lobby class
+    private String sessionId; // Session ID for the current player
+
+    @FXML
+    private TextField roomCodeField;
+    @FXML
+    private TextArea RULES;
+    
+    public void initialize() {
+        lobby = new Lobby(); // Instantiate the Lobby class
+        int userId = lobby.generateUserId();
+        sessionId = generateSessionId(userId); // Generate session ID for the player
+        System.out.println("Welcome! Your User ID: " + userId + ", Session ID: " + sessionId);
+        String paragraph =  """
+                            Steps:
+                            1. Once 7 players have entered the game room, the room will be start.
+                            2. Count down starts from 3 and then, the game begins.
+                            3. Roles are assigned to each of the players randomly. Villagers can only see their own roles, while werewolves can see their roles as well as the other werewolves.
+                            4. When the night phase begins, the werewolves will secretly discuss and select a player to be eliminated.
+                            5. Other villagers with abilities such as doctor and seer can also use their powers during the night phase. However, the seer can only use her ability on the second night.
+                            6. The day phase begins, all players will discuss and try to identify werewolves among them within 2 minutes.
+                            7. Players with the most vote will be eliminated. Their role will be revealed to determine whether they were a villager or a werewolf.
+                            
+                            Roles:
+                            i. Villager: Has no special ability.
+                            ii. Seer: Can identify the role of one of the players (villagers or werewolves.)
+                            iii. Doctor: Can protect one of the players at night from being eliminated.
+                            iv. Werewolf: Is able to kill innocent villagers at night.""";
+        RULES.setText(paragraph);
+    }
+    
+    @FXML
+    private void joinRoomButtonClicked(ActionEvent event) {
+        //can get the user id from the database
+        lobby.getUserId();
+        int userId = lobby.getUserId();
+        lobby.joinPublicRoom(userId);
+        System.out.println("Joined public room. User ID: " + userId);
+    }
+
+    @FXML
+    private void createRoomButtonClicked(ActionEvent event) {
+        //can get the user id from the database
+        int userId = lobby.getUserId();
+        String roomCode = lobby.createPrivateRoom(userId);
+        if (roomCode != null) {
+            System.out.println("Created private room. User ID: " + userId + ", Room Code: " + roomCode);
+        } else {
+            System.out.println("Failed to create private room. Maximum number of private rooms reached.");
+        }
+    }
+    
+    @FXML
+    private void joinWithCodeButtonClicked(ActionEvent event) {
+        String roomCode = roomCodeField.getText();
+        int userId = lobby.getUserId();
+        // TODO: Implement logic to join private room with code
+        if (!roomCode.isEmpty()) {
+            System.out.println("Joined private room with code: " + roomCode + ". User ID: " + userId);
+        } else {
+            System.out.println("Please enter a valid room code.");
+        }
+    }
+    
+    private String generateSessionId(int userId) {
+        // Generate session ID based on user ID or any other logic you prefer
+        return "SESSION_" + userId;
+    }
+    /**
+     * Initializes the controller class.
+     */
+//    @Override
+//    public void initialize(URL url, ResourceBundle rb) {
+//        // TODO
+//    }    
+    
+}
